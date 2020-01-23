254855d007cc8d1b47124c2ca2015ae9df7802b9
ProfPaulB <paulbonenfant@hotmail.com>
2020-01-23 14:43:25 -0500

    First commit of the project

---
+++ src/gitreview2/GitReview2.java
@@ -0,0 +1,20 @@
+package gitreview2;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Paul Bonenfant
+ */
+public class GitReview2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Git Practice");
+
+    }
+
+}
+
